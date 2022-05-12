a6260ce522a94a72ed4e2ebdfe9b5ea5221b6418
Glitch (hello-express) <none>
2022-05-12 15:14:45 +0000

    🏃👑 Checkpoint ./app.js:44746533/1981

--- app.js
+++ app.js
@@ -3,32 +3,30 @@ const { App, subtype } = require("@slack/bolt");
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
-
-
-app.event('message', async ({client, message}) => {
+app.event("message", async ({ client, message }) => {
   try {
-    
-    if (message && message.text.includes("Google 'Festinger Vault' and let's get started!")){
-          const messageId = message.ts
-          const channelId = message.channel
-          
-          try {
-              // Call the chat.delete method using the WebClient
-              const result = await client.chat.delete({
-                token:process.env.USER_TOKEN,
-                channel: channelId,
-                ts: messageId
-              });
-            }
-            catch (error) {
-              console.error(error);
-            }
+    if (
+      message &&
+      message.text.includes("Google 'Festinger Vault' and let's get started!")
+    ) {
+      const messageId = message.ts;
+      const channelId = message.channel;
+
+      try {
+        // Call the chat.delete method using the WebClient
+        const result = await client.chat.delete({
+          token: process.env.USER_TOKEN,
+          channel: channelId,
+          ts: messageId,
+        });
+      } catch (error) {
+        console.error(error);
+      }
     }
-  }
-  catch (error) {
+  } catch (error) {
     console.error(error);
   }
 });
@@ -37,5 +35,5 @@ app.event('message', async ({client, message}) => {
   // Start your app
   await app.start(process.env.PORT || 3000);
 
-  console.log('⚡️ Bolt app is running!');
+  console.log("⚡️ Bolt app is running!");
 })();

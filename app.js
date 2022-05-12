1aef94d11999061b55bb39a5e281c124d974c3fb
Glitch (hello-express) <none>
2022-05-12 11:25:04 +0000

    💡📠 Checkpoint ./app.js:44746533/1500

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.event('app_home_opened', async ({ event, client, context }) => {
             "type": "section",
             "text": {
               "type": "mrkdwn",
-              "text": "*Welcome to your _App's Home_* :tada:"
+              "text": "*askflaksdjnfklasdjnf to your _App's Home_* :tada:"
             }
           },
           {
@@ -57,59 +57,18 @@ app.event('app_home_opened', async ({ event, client, context }) => {
         ]
       }
     });
+      console.log("event")
   }
+
+  
   catch (error) {
     console.error(error);
   }
 });
 
-app.event('message', async ({ event, client, context }) => {
+app.event('message', async ({ type, channel, text, user }) => {
   try {
-    /* view.publish is the method that your app uses to push a view to the Home tab */
-    const result = await client.views.publish({
-
-      /* the user that opened your app's app home */
-      user_id: event.user,
-
-      /* the view object that appears in the app home*/
-      view: {
-        type: 'home',
-        callback_id: 'home_view',
-
-        /* body of the view */
-        blocks: [
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "*Welcome to your _App's Home_* :tada:"
-            }
-          },
-          {
-            "type": "divider"
-          },
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
-            }
-          },
-          {
-            "type": "actions",
-            "elements": [
-              {
-                "type": "button",
-                "text": {
-                  "type": "plain_text",
-                  "text": "Click me!"
-                }
-              }
-            ]
-          }
-        ]
-      }
-    });
+    console.log(text)
   }
   catch (error) {
     console.error(error);

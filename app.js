6f6675f6fc3b4424a64eedec56bfc6c63d64eb29
Glitch (hello-express) <none>
2022-05-12 11:59:07 +0000

    🌱☺️ Checkpoint ./app.js:44746533/6735

--- app.js
+++ app.js
@@ -8,70 +8,26 @@ const app = new App({
 
 
 
-// All the room in the world for your code
-
-app.event('app_home_opened', async ({ event, client, context }) => {
-  try {
-    /* view.publish is the method that your app uses to push a view to the Home tab */
-    const result = await client.views.publish({
-
-      /* the user that opened your app's app home */
-      user_id: event.user,
-
-      /* the view object that appears in the app home*/
-      view: {
-        type: 'home',
-        callback_id: 'home_view',
-
-        /* body of the view */
-        blocks: [
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "*askflaksdjnfklasdjnf to your _App's Home_* :tada:"
-            }
-          },
-          {
-            "type": "divider"
-          },
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
-            }
-          },
-          {
-            "type": "actions",
-            "elements": [
-              {
-                "type": "button",
-                "text": {
-                  "type": "plain_text",
-                  "text": "Click me!"
-                }
-              }
-            ]
-          }
-        ]
-      }
-    });
-    console.log("event")
-  }
-
-  
-  catch (error) {
-    console.error(error);
-  }
-});
-
-
-
-app.event('message', async ({message}) => {
+app.event('message', async ({client, message}) => {
   try {
     console.log(message)
-    if (message.text in)
+    if (message.text.includes("test")){
+          const messageId = message.ts
+          const channelId = message.channel
+          
+          try {
+              // Call the chat.delete method using the WebClient
+              const result = await client.chat.delete({
+                channel: channelId,
+                ts: messageId
+              });
+
+              console.log(result);
+            }
+            catch (error) {
+              console.error(error);
+            }
+    }
   }
   catch (error) {
     console.error(error);
